8e2eabb14651246b8e996ede842c9d82bf7bffae
Aaron Kaye <ajk292@cornell.edu>
2021-10-13 22:25:20 -0400

    Fixing an issue with useEffect and dependencies.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import Amiibos from "./components/amiibos";
 
 const API_BASE = "https://amiiboapi.com/api/";
 
+const fetchFromApi = async (apiEndpoint) => {
+  const response = await fetch(`${API_BASE}${apiEndpoint}`);
+  return response.json();
+};
+
 function App() {
   const [amiibos, setAmiibos] = useState([]);
 
-  useEffect(() => {
-    loadAmiibos();
-  }, []);
-
-  const loadAmiibos = async () => {
-    fetchAmiibos().then((data) => {
+  const loadAmiibos = useCallback(() => {
+    fetchFromApi("/amiibo/?type=figure").then((data) => {
       setAmiibos(data.amiibo);
     });
-  };
+  }, []);
 
-  const fetchAmiibos = async () => {
-    const apiEndpoint = "/amiibo/?type=figure";
-    const response = await fetch(`${API_BASE}${apiEndpoint}`);
-    return response.json();
-  };
+  useEffect(() => {
+    loadAmiibos();
+  }, [loadAmiibos]);
 
   return <Amiibos amiibos={amiibos} />;
 }
